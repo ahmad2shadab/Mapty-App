559f490e3428778c4a3ddc63abe3f066453ee61c
Shadab Ahmad <122364473+ahmad2shadab@users.noreply.github.com>
2023-02-14 01:52:01 +0530

    Managing workout data: Creating Classes
    Creating Workout, Running and Cycling classes

--- script.js
+++ script.js
@@ -11,6 +11,48 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elavationGain) {
+    super(coords, distance, duration);
+    this.elavationGain = elavationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+  }
+}
+
+// const running = new Running([39, -12], 5.2, 24, 178);
+// const cycling = new Cycling([39, -12], 27, 95, 543);
+// console.log(running, cycling);
+
+/* <--.....----......----.....--> */
+// APPLICATION ARCHITECTURE
 class App {
   #map;
   #mapEvent;
